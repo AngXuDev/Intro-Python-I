fd5a32ff441e68fa51244432a84be8e135ceb119
Ang Xu <angxu1234@gmail.com>
2019-09-30 17:24:01 -0500

    00 through 07 completed

--- src/04_printing.py
+++ src/04_printing.py
@@ -4,6 +4,8 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
+import decimal
+
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -11,7 +13,10 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("x is %s, y is %s, and z is \"%s\"" % (x, round(y, 2), z))
 
 # Use the 'format' string method to print the same thing
+print("x is {}, y is {}, and z is \"{}\"".format(x, round(y, 2), z))
 
 # Finally, print the same thing using an f-string
+print(f"x is {x}, y is {round(y, 2)}, and z is \"{z}\"")
