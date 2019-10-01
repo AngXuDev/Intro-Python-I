e7b11717037c8da2eaa40110b8fe5a2407f9988a
Ang Xu <angxu1234@gmail.com>
2019-10-01 18:10:33 -0500

    updated module 14

--- src/14_cal.py
+++ src/14_cal.py
@@ -23,14 +23,22 @@ import sys
 import calendar
 from datetime import datetime
 
+# if len(sys.argv) > 3:
+#     print(f"Please provide arguments in the format 'Python 14_cal.py (month) (year)' where month is between 01-12")
+# elif len(sys.argv) == 3:
+#     m = int(sys.argv[1])
+#     y = int(sys.argv[2])
+#     print(calendar.month(y, m))
+# elif len(sys.argv) == 2:
+#     m = int(sys.argv[1])
+#     print(calendar.month(datetime.today().year, m))
+# elif len(sys.argv) == 1:
+#     print(calendar.month(datetime.today().year, datetime.today().month))
+
+m = int(sys.argv[1]) if len(sys.argv) > 1 else datetime.today().month
+y = int(sys.argv[2]) if len(sys.argv) > 2 else datetime.today().year
+
 if len(sys.argv) > 3:
     print(f"Please provide arguments in the format 'Python 14_cal.py (month) (year)' where month is between 01-12")
-elif len(sys.argv) == 3:
-    m = int(sys.argv[1])
-    y = int(sys.argv[2])
+else:
     print(calendar.month(y, m))
-elif len(sys.argv) == 2:
-    m = int(sys.argv[1])
-    print(calendar.month(2019, m))
-elif len(sys.argv) == 1:
-    print(calendar.month(2019, 10))
